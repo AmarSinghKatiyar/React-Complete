85c6916444ed15f9644fe679d59b2a315124f489
AmarSinghKatiyar <amarsinghkatiyar1408@gmail.com>
2026-06-13 01:56:24 +0530

    Components complete

---
+++ 02-Components/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Card from './Components/Card'
+
+const App = () => {
+  return (
+    <div className='main'>
+      <Navbar />
+      <div className="hero">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>    
+    </div>
+  )
+}
+
+export default App
---
+++ 02-Components/src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='card'>
+        <h1>This is a card</h1>
+        <img className='img' src="https://th.bing.com/th/id/OIP.5NL3IKh0XrogPy_yGj2l-AHaHa?w=173&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="logo" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, vel.
+        </p>
+    </div>
+  )
+}
+
+export default card
