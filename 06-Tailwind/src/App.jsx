88fec770884bb8e4ac63fd0160011d6a5dcdbf0f
AmarSinghKatiyar <amarsinghkatiyar1408@gmail.com>
2026-06-13 17:20:37 +0530

    Using Tailwind with React

---
+++ 06-Tailwind/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-800 text-white text-7xl'>Hello How are you ???</div>
+  )
+}
+
+export default App
